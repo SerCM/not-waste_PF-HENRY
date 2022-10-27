7ced6288da722f4a82f85b12872287bd6ba161ed
SerCM <serranocristian@hotmail.com.ar>
2022-10-27 09:38:51 -0300

    123

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -81,7 +81,7 @@ router.get("/diets", getDiets);
 
 //mercadopago -->
 router.post("/create_preference", post_create_preference);
-router.get("/feedback", get_feedback);
+router.post("/feedback", get_feedback);
 //<-- mercadopago
 
 module.exports = router;
