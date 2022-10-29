54ecb15aae1aba553dd9b244033c45398e351859
Sofia Caria <103151285+socaria@users.noreply.github.com>
2022-10-29 12:01:30 +0200

    dashboard usuarios registrados

--- client/src/component/Dashboard/Dashboard.jsx
+++ client/src/component/Dashboard/Dashboard.jsx
@@ -5,13 +5,41 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 import { getCustomer, getSellers } from "../../redux/actions";
+import { PieChart, Pie, Sector, Cell, ResponsiveContainer, BarChart, Bar, XAxis, YAxis } from 'recharts';
 import DashboardUsersRegistered from "../DashboardUsersRegistered/DashboardUsersRegistered";
+import DashboardCategories from "../DashboardCategorySeller/DashboardCategories";
+
 
 function Dashboard() {
+    const dispatch = useDispatch();
+    const customers = useSelector(state => state.customer)
+    const sellers = useSelector(state => state.seller)
+
+    console.log("🚀 ~ file: Dashboard.jsx ~ line 14 ~ Dashboard ~ customers", customers)
+    useEffect(() => {
+        dispatch(getCustomer());
+        dispatch(getSellers());
+    }, [dispatch]);
+
+
+    const data = [
+        { name: 'Proveedores', value: sellers?.length },
+        { name: 'Consumidores', value: customers?.length },
+    ];
+    const COLORS = ['#FFBB28', '#FF8042'];
+
+    // '#0088FE', '#00C49F', 
+    // static demoUrl = 'https://codesandbox.io/s/pie-chart-with-padding-angle-7ux0o';
+
+
     return (
 
         <div>
-            <DashboardUsersRegistered/>
+            <h1>Dashboard Admin</h1>
+            <DashboardUsersRegistered customers={customers} sellers={sellers} />
+            <DashboardCategories />
+            {/* <DashboardOrders />
+            <DashboardCumpliments /> */}
         </div>
     )
 }
--- client/src/component/DashboardUsersRegistered/DashboardUsersRegistered.jsx
+++ client/src/component/DashboardUsersRegistered/DashboardUsersRegistered.jsx
@@ -7,30 +7,17 @@ import { useEffect } from "react";
 import { getCustomer, getSellers } from "../../redux/actions";
 import { PieChart, Pie, Sector, Cell, ResponsiveContainer, BarChart, Bar, XAxis, YAxis } from 'recharts';
 
-function DashboardUsersRegistered() {
-    const dispatch = useDispatch();
-    const customers = useSelector(state => state.customer)
-    const sellers = useSelector(state => state.seller)
-
-    useEffect(() => {
-        dispatch(getCustomer());
-        dispatch(getSellers());
-    }, [dispatch]);
+function DashboardUsersRegistered({customers, sellers}) {
 
     const data = [
         { name: 'Proveedores', value: sellers?.length },
         { name: 'Consumidores', value: customers?.length },
     ];
     const COLORS = ['#FFBB28', '#FF8042'];
-
-    // '#0088FE', '#00C49F', 
-    // static demoUrl = 'https://codesandbox.io/s/pie-chart-with-padding-angle-7ux0o';
-
-
     return (
 
         <div>
-            <h1>Dashboard Admin</h1>
+            
             <div className="containerCarousel">
                 <h2 className="sellerTitle">Usuarios registrados </h2>
                 <PieChart width={400} height={200} onMouseEnter={PureComponent.onPieEnter}>
@@ -44,8 +31,8 @@ function DashboardUsersRegistered() {
                         paddingAngle={5}
                         dataKey="value"
                     >
-                        {data.map((entry, index) => (
-                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                        {data?.map((entry, index) => (
+                            <Cell key={`cell-${index}`} fill={COLORS[index % COLORS?.length]} />
                         ))}
                     </Pie>
                 </PieChart>
