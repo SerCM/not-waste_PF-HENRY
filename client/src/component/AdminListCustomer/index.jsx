36f688fb58a638b3680ffdb7cd0c58b867a9c6e2
gabyakd2 <gabrielsal00@hotmai.com>
2022-10-31 15:09:33 -0300

    ruta nueva, comp adminlistcustomer

---
+++ client/src/component/AdminListCustomer/index.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import AuthProfile from "../AuthProfile";
+import VerifyProfile from "../VerifyProfile";
+
+import NavBar from "../NavBar/index";
+import Footer from "../Footer/index";
+
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+
+export default function AdminListCustomer() {
+    return (
+        <div>
+            <NavBar />
+            <Footer />
+        </div>
+    )
+}
